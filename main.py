660b71338be75408de2741f266ead611bddac666
Milan <milangru@gmail.com>
2024-04-02 19:12:40 +0200

    Minor changes on variable names, docstring, empty trailing last line

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 import hashlib
 
 def reverse_hash(hash, wordlist):
+    '''Compares the provided hash with the hashed passwords from wordlist.'''
     for password in wordlist.split("\n"):
         guess = hashlib.sha3_512(bytes(password, "utf-8")).hexdigest()
         if guess == hash:
@@ -12,7 +13,7 @@ def reverse_hash(hash, wordlist):
         else:
             print("The password does not matched in the list…")
 
-password_list = str(
+ten_million_password_list = str(
     urlopen(
         "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
     ).read(),
@@ -21,4 +22,4 @@ password_list = str(
 
 sha3_512_hash = input("[+] Enter sha3-512 hash value: ")
 
-reverse_hash(sha3_512_hash, password_list)
+reverse_hash(sha3_512_hash, ten_million_password_list)
