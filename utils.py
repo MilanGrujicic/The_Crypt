e10063b3500b501c1ea2e32028533d1952a8fa74
Milan <milangru@gmail.com>
2024-05-07 22:50:29 +0200

    Moved functions to a dedicated file.

---
+++ utils.py
@@ -0,0 +1,40 @@
+from urllib.request import urlopen
+import hashlib
+import sys
+import time
+
+def load_spinner():
+    '''Displays a spinner animation using basic ASCII characters.'''
+    for char in '|/-\\':
+        sys.stdout.write('\r' + char)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+def load_wordlist():
+    '''Loads a 10 million password list.'''
+    print("[-] Loading wordlist... Please wait.")
+    ten_million_password_list = str(
+        urlopen(
+            "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
+        ).read(),
+        "utf-8",
+    )
+    return ten_million_password_list
+
+def reverse_hash(hash):
+    '''Compares the provided hash with the hashed passwords from wordlist.'''
+    wordlist = load_wordlist()
+    password_not_found = True
+    print("\n[-] Checking passwords... Please wait.")
+    while password_not_found:
+        load_spinner()
+        for password in wordlist.split("\n"):
+            guess = hashlib.sha3_512(bytes(password, "utf-8")).hexdigest()
+            if guess == hash:
+                print("\n[+] The password is: " + str(password))
+                password_not_found = False
+                break
+            elif guess != hash:
+                continue
+            else:
+                print("\nPassword not found.")
