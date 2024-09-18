50a2a850fee241f2d52281f79c5037124f3b6c34
Milan <milangru@gmail.com>
2024-09-18 21:03:38 +0200

    Added exit feature.

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import utils
 
 print(logo.logo)
 
+print("Type 'c' to exit.\n")
+
 sha3_512_hash = input("[+] Enter sha3-512 hash value: ")
+
+if sha3_512_hash in 'Cc':
+    import sys
+    sys.exit()
 
 utils.reverse_hash(sha3_512_hash)
